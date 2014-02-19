ea1fd6ea78a86cb5f8586316dc2b4cd7465a459d
ShevchukevychO <Oleksiy.Shevchukevych@tutor.com>
2014-02-18 23:12:48 -0500

    - using real "Integrated Security = False"
    - made ASP.NET Identity working with Entity Framework

--- SparWeb/Models/IdentityModels.cs
+++ SparWeb/Models/IdentityModels.cs
@@ -10,7 +10,7 @@ namespace SparWeb.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+			: base("SparConnection")
         {
         }
     }
