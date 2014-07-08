ed3270bb04b62d719da53e293f01bd1f223f7e91
ShevchukevychO <Oleksiy.Shevchukevych@tutor.com>
2014-07-07 23:55:56 -0400

    taking it easy on password validation; the only requirement is minimum 6 characters

--- SparWeb/App_Start/IdentityConfig.cs
+++ SparWeb/App_Start/IdentityConfig.cs
@@ -38,10 +38,10 @@ namespace SparWeb.Models
 			manager.PasswordValidator = new PasswordValidator
 			{
 				RequiredLength = 6,
-				RequireNonLetterOrDigit = true,
-				RequireDigit = true,
-				RequireLowercase = true,
-				RequireUppercase = true,
+				RequireNonLetterOrDigit = false,
+				RequireDigit = false,
+				RequireLowercase = false,
+				RequireUppercase = false,
 			};
 			// Configure user lockout defaults
 			manager.UserLockoutEnabledByDefault = true;
