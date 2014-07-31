2edcc5e9649c8e7124823bdee94aaad5fc106cb0
ShevchukevychO <Oleksiy.Shevchukevych@tutor.com>
2014-07-31 00:37:04 -0400

    good progress on Spar Confirmation page

---
+++ SparWeb/Controllers/SparController.cs
@@ -0,0 +1,53 @@
+﻿using SparData;
+using SparModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using SparWeb.Models;
+
+namespace SparWeb.Controllers
+{
+    public class SparController : Controller
+    {
+        //
+        // GET: /Fighter/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[Authorize]
+		public ActionResult SparConfirmation(string ID)
+		{
+			FighterRepository fighterRepo = new FighterRepository();
+			
+			Fighter thisFighter = fighterRepo.GetFighterByIdentityUserId(User.Identity.GetUserId());
+			AccountViewModel thisFighterAccountViewModel = Util.GetAccountViewModelForFighter(thisFighter, 250);
+
+			Fighter opponentFighter = fighterRepo.GetFighterByIdentityUserId(ID);
+			AccountViewModel opponentFighterAccountViewModel = Util.GetAccountViewModelForFighter(opponentFighter, 250);
+
+			SparConfirmationViewModel sparConfirmationViewModel = new SparConfirmationViewModel()
+			{
+				ThisFighterID = thisFighterAccountViewModel.ID,
+				ThisFighterName = thisFighterAccountViewModel.Name,
+				ThisFighterGymName = thisFighterAccountViewModel.GymName,
+				ThisProfilePictureFile = thisFighterAccountViewModel.ProfilePictureFile,
+				ThisProfilePictureUploaded = thisFighterAccountViewModel.ProfilePictureUploaded,
+				OpponentFighterID = opponentFighterAccountViewModel.ID,
+				OpponentFighterName = opponentFighterAccountViewModel.Name,
+				OpponentFighterGymName = opponentFighterAccountViewModel.GymName,
+				OpponentProfilePictureFile = opponentFighterAccountViewModel.ProfilePictureFile,
+				OpponentProfilePictureUploaded = opponentFighterAccountViewModel.ProfilePictureUploaded
+			};
+
+			return View(sparConfirmationViewModel);
+		}
+	}
+}
---
+++ SparWeb/Models/SparViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparWeb.Models
+{
+	public class SparConfirmationViewModel
+	{
+		public string ThisFighterID { get; set; }
+		public string ThisFighterName { get; set; }
+		public string ThisFighterGymName { get; set; }
+		public string ThisProfilePictureFile { get; set; }
+		public bool ThisProfilePictureUploaded { get; set; }
+		public string OpponentFighterID { get; set; }
+		public string OpponentFighterName { get; set; }
+		public string OpponentFighterGymName { get; set; }
+		public string OpponentProfilePictureFile { get; set; }
+		public bool OpponentProfilePictureUploaded { get; set; }
+	}
+}
