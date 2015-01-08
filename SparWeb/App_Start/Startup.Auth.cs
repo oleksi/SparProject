2b9953c8beed1940f5076964be2a70fde6a5282a
ShevchukevychO <oleksiy.shevchukevych@tutor.com>
2015-01-07 22:56:37 -0500

    when non-authenticated users tries to access restricted area, send him to the login screen

--- SparWeb/App_Start/Startup.Auth.cs
+++ SparWeb/App_Start/Startup.Auth.cs
@@ -25,7 +25,7 @@ namespace SparWeb
 			app.UseCookieAuthentication(new CookieAuthenticationOptions
 			{
 				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-				LoginPath = new PathString("/Account/Register"),
+				LoginPath = new PathString("/Account/Login"),
 				Provider = new CookieAuthenticationProvider
 				{
 					// Enables the application to validate the security stamp when the user logs in.
