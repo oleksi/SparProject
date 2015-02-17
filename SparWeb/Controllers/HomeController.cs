a478cf6ef4e74fc6c8f9eb43d19b2d36bfa6927a
ShevchukevychO <oleksiy.shevchukevych@tutor.com>
2015-02-16 20:57:23 -0500

    implemented age filter

--- SparWeb/Controllers/HomeController.cs
+++ SparWeb/Controllers/HomeController.cs
@@ -20,28 +20,47 @@ namespace SparWeb.Controllers
 			FighterRepository fighterRepo = new FighterRepository();
 			SparRepository sparRepo = new SparRepository();
 
-			int loggedInFighterId = -1;
-			if (User.Identity.GetUserId() != null)
-				loggedInFighterId = fighterRepo.GetFighterByIdentityUserId(User.Identity.GetUserId()).Id.Value;
-
 			IList<Fighter> fightersList = fighterRepo.GetAllFighters();
 
-			List<AccountViewModel> fightersAccountViewModelList = new List<AccountViewModel>();
-			foreach(Fighter currFighter in fightersList)
+			HomeViewModel model = new HomeViewModel();
+			model.FightersList = getFightersListViewModel(fightersList);
+			model.Sex = true;
+
+			ViewBag.AgeRange = Util.AgeRangeMap;
+			ViewBag.WeightClassMap = Util.WeightClassMap;
+			ViewBag.HeightToCentimetersMap = Util.HeightToCentimetersMap;
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public ActionResult Index(HomeViewModel model)
+		{
+			FighterRepository fighterRepo = new FighterRepository();
+			IList<Fighter> fightersList = null;
+
+			if (model.AgeRange != null)
 			{
-				if (loggedInFighterId == -1 || loggedInFighterId != currFighter.Id)
-				{
-					AccountViewModel accountViewModel = Util.GetAccountViewModelForFighter(currFighter, 150);
-
-					if (User.Identity.IsAuthenticated == true)
-						accountViewModel.SparRequests = Util.GetSparRequestDetailsForFighter(currFighter.Id.Value, User.Identity.GetUserId()).Where(sr => sr.OpponentFighter.Id == currFighter.Id).ToList();
-
-					fightersAccountViewModelList.Add(accountViewModel);
-				}
+				if (model.AgeRange == AgeRange.AgeBelow12)
+					fightersList = fighterRepo.GetAllFighters().Where(ff => getAge(ff.DateOfBirth) < 12).ToList();
+				else if (model.AgeRange == AgeRange.Between12And16)
+					fightersList = fighterRepo.GetAllFighters().Where(ff => getAge(ff.DateOfBirth) >= 12 && getAge(ff.DateOfBirth) < 16).ToList();
+				else if (model.AgeRange == AgeRange.Between16And20)
+					fightersList = fighterRepo.GetAllFighters().Where(ff => getAge(ff.DateOfBirth) >= 16 && getAge(ff.DateOfBirth) < 20).ToList();
+				else if (model.AgeRange == AgeRange.Between20And24)
+					fightersList = fighterRepo.GetAllFighters().Where(ff => getAge(ff.DateOfBirth) >= 20 && getAge(ff.DateOfBirth) < 24).ToList();
+				else if (model.AgeRange == AgeRange.Between24And30)
+					fightersList = fighterRepo.GetAllFighters().Where(ff => getAge(ff.DateOfBirth) >= 24 && getAge(ff.DateOfBirth) < 30).ToList();
+				else if (model.AgeRange == AgeRange.Between30And36)
+					fightersList = fighterRepo.GetAllFighters().Where(ff => getAge(ff.DateOfBirth) >= 30 && getAge(ff.DateOfBirth) < 36).ToList();
+				else if (model.AgeRange == AgeRange.Above36)
+					fightersList = fighterRepo.GetAllFighters().Where(ff => getAge(ff.DateOfBirth) >= 36).ToList();
 			}
 
-			HomeViewModel model = new HomeViewModel();
-			model.FightersList = fightersAccountViewModelList;
+			if (fightersList == null)
+				fightersList = fighterRepo.GetAllFighters();
+
+			model.FightersList = getFightersListViewModel(fightersList);
 
 			ViewBag.AgeRange = Util.AgeRangeMap;
 			ViewBag.WeightClassMap = Util.WeightClassMap;
@@ -62,6 +81,41 @@ namespace SparWeb.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		private List<AccountViewModel> getFightersListViewModel(IList<Fighter> fightersList)
+		{
+			FighterRepository fighterRepo = new FighterRepository();
+
+			int loggedInFighterId = -1;
+			if (User.Identity.GetUserId() != null)
+				loggedInFighterId = fighterRepo.GetFighterByIdentityUserId(User.Identity.GetUserId()).Id.Value;
+
+			List<AccountViewModel> fightersAccountViewModelList = new List<AccountViewModel>();
+			foreach (Fighter currFighter in fightersList)
+			{
+				if (loggedInFighterId == -1 || loggedInFighterId != currFighter.Id)
+				{
+					AccountViewModel accountViewModel = Util.GetAccountViewModelForFighter(currFighter, 150);
+
+					if (User.Identity.IsAuthenticated == true)
+						accountViewModel.SparRequests = Util.GetSparRequestDetailsForFighter(currFighter.Id.Value, User.Identity.GetUserId()).Where(sr => sr.OpponentFighter.Id == currFighter.Id).ToList();
+
+					fightersAccountViewModelList.Add(accountViewModel);
+				}
+			}
+
+			return fightersAccountViewModelList;
+		}
+
+		private int getAge(DateTime dob)
+		{
+			DateTime now = DateTime.Today;
+
+			int age = now.Year - dob.Year;
+			if (dob > now.AddYears(-age)) age--;
+
+			return age;
 		}
 	}
 }
