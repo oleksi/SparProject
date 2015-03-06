775875f176e629ce973ee8fdb0862eb29f91682e
ShevchukevychO <oleksiy.shevchukevych@tutor.com>
2015-03-05 23:47:28 -0500

    confirm your email is real deal now

--- SparWeb/Controllers/AccountController.cs
+++ SparWeb/Controllers/AccountController.cs
@@ -214,7 +214,7 @@ namespace SparWeb.Controllers
 					var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
 					var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
 					await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
-					ViewBag.Link = callbackUrl;
+
 					return View("DisplayEmail");
                 }
                 else
