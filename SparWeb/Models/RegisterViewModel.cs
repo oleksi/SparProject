bcd6d7092646c15c588e418398c1261718b04e82
ShevchukevychO <oleksiy.shevchukevych@tutor.com>
2015-08-24 23:28:17 -0400

    removed "are you trainer/fighter?" question from registration; fixed trainer/fighter registration inseide popup

---
+++ SparWeb/Models/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparWeb.Models
+{
+	public class RegisterMainViewModel
+	{
+		public bool IsPopupMode { get; set; }
+	}
+}
