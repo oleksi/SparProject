d765d343c817ba884d24c2b0a5afbda50883f475
ShevchukevychO <oleksiy.shevchukevych@tutor.com>
2015-10-29 00:44:35 -0400

    expired spars logic on account and spar details page

--- SparData/SparRepository.cs
+++ SparData/SparRepository.cs
@@ -69,8 +69,8 @@ namespace SparData
 			using (var session = getSession())
 			{
 				sparRequests = session.QueryOver<SparRequest>().Where(sr => (sr.RequestorFighter.Id == fighterId || sr.OpponentFighter.Id == fighterId)
-						&& ((sr.Status == SparRequestStatus.Confirmed && sr.SparDateTime > DateTime.Now)
-							|| ((sr.Status == SparRequestStatus.DateLocationNegotiation || sr.Status == SparRequestStatus.Requested) && sr.RequestDate > DateTime.Now.AddDays(-14))
+						&& ((sr.Status == SparRequestStatus.DateLocationNegotiation && sr.SparDateTime > DateTime.Now.AddDays(1))
+							|| (sr.Status == SparRequestStatus.Requested && sr.RequestDate > DateTime.Now.AddDays(-14))
 						)
 					).OrderBy(sr => sr.RequestDate).Desc.List();
 			}
