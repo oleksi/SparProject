ea437db0666f895e575d2de6b5fca93fab4721ee
ShevchukevychO <oleksiy.shevchukevych@tutor.com>
2016-07-22 01:17:04 -0400

    upd

--- SparWeb/Models/Util.cs
+++ SparWeb/Models/Util.cs
@@ -428,7 +428,7 @@ namespace SparWeb
 
 		public static bool IsRegistrationPopupAllowed(HttpRequestBase request)
 		{
-			return (request.IsAuthenticated == false && request.RawUrl.ToLower().EndsWith("/register") == false && request.RawUrl.ToLower().EndsWith("/register#") == false);
+			return (request.IsAuthenticated == false && request.RawUrl.ToLower().IndexOf("/register") == -1 && request.RawUrl.ToLower().IndexOf("/register#") == -1);
 		}
 	}
 }
